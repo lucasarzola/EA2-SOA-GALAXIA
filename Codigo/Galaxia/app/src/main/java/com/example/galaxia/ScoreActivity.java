ddb2d23426559256fc51a483d75166ec0c4f24a2
lucasarzola <lucas.arzola@gmail.com>
2020-11-16 16:13:35 -0300

    Correcion en la pantalla de Score

--- Codigo/Galaxia/app/src/main/java/com/example/galaxia/ScoreActivity.java
+++ Codigo/Galaxia/app/src/main/java/com/example/galaxia/ScoreActivity.java
@@ -1,6 +1,7 @@
 package com.example.galaxia.model;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,12 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.galaxia.EventosActivity;
+import com.example.galaxia.GameActivity;
 import com.example.galaxia.R;
+import com.example.galaxia.servicios.ServicioHTTP;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Score  extends AppCompatActivity implements View.OnClickListener {
@@ -19,7 +25,7 @@ public class Score  extends AppCompatActivity implements View.OnClickListener {
     private TextView mScore, mEnemigo, mPuntaje;
     private Button btnEvento;
     private LinearLayout mHighScoreContainer;
-
+    private  String token ="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,19 +38,23 @@ public class Score  extends AppCompatActivity implements View.OnClickListener {
         btnEvento = findViewById( R.id.btnEvento );
         mHighScoreContainer = findViewById(R.id.high_score_container);
         mBack.setOnClickListener(this);
-
+        Bundle extras = getIntent().getExtras();
+        String token = extras.getString( "token" );
         cargarPuntajeMasAlto();
         btnEvento.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Score.this, EventosActivity.class);
+                informarEvento( "Cambio de Activity", "Se abrio pantalla de Eventos" );
                 startActivity(i);
+
             }
         });
 
     }
 
     void cargarPuntajeMasAlto(){
+        informarEvento( "PantalladeScore","Se encuentra en pantalla de Score" );
         SharedPreferencesManager spm = new SharedPreferencesManager(this);
         if (spm.getPuntajeAlto()!=-1){
             mPuntaje.setVisibility(TextView.GONE);
@@ -64,5 +74,43 @@ public class Score  extends AppCompatActivity implements View.OnClickListener {
                 finish();
                 break;
         }
+    }
+
+    public void informarEvento(String tipoEvento, String descripcion){
+
+        token = getSharedPreferences( "tokenDeSesion" ,MODE_PRIVATE).getString("token","").toString();
+
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("env", "PROD");
+            obj.put("type_events", tipoEvento);
+            obj.put("description", descripcion);
+            Intent i = new Intent( Score.this, ServicioHTTP.class);
+            i.putExtra("uri", "http://so-unlam.net.ar/api/api/event");
+            i.putExtra("token", token);
+            i.putExtra("datosJson", obj.toString());
+
+            startService(i);
+            guardarEnPreferences(obj.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void guardarEnPreferences(String datosEvento){
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("HistorialEventosPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+
+        int cantEventos=pref.getInt("cantEventos", 0);
+        cantEventos++;
+
+        String datos="IdEvento "+String.valueOf(cantEventos) + datosEvento;
+        editor.putInt("cantEventos", cantEventos);
+        editor.putString("key"+String.valueOf(cantEventos), datos);
+        editor.commit();
+
     }
 }
